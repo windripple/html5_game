076afb69e06a8429c9131d31971d1279dce3f2f1
windripple <york11122@gmail.com>
2014-02-24 13:27:10 +0800

    html5 game
    2011年專題檔案 紀錄

---
+++ html5_game (5)/htf.js
@@ -0,0 +1,30 @@
+﻿		var ctx;
+		var stopGame = false;
+		function runGame(){			
+			ctx = $('#cvs').get(0).getContext('2d');
+			stopGame = false;
+			initial();
+			runtime();
+		}
+		
+		function runtime(){
+			update();
+			draw();
+			if(!stopGame) setTimeout("runtime()",16);
+			else onExit();
+		}
+		
+		function draw(){
+			//override this function
+		}	
+		
+		function update(){
+			//override this function
+		}
+		
+		
+		function initial(){		
+		}
+		
+		function onExit(){
+		}
